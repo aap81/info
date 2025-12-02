4603487962682dddb66898fb835005ac4b9c4ddf
Aaron P <alexaaron120@gmail.com>
2025-12-01 17:47:59 -0800

    reset

---
+++ src/containers/profile/Profile.js
@@ -0,0 +1,15 @@
+import React, {lazy, Suspense} from "react";
+import {openSource} from "../../portfolio";
+import Loading from "../loading/Loading";
+
+const renderLoader = () => <Loading />;
+const GithubProfileCard = lazy(() =>
+  import("../../components/githubProfileCard/GithubProfileCard")
+);
+export default function Profile() {
+  return (
+    <Suspense fallback={renderLoader()}>
+      <GithubProfileCard/>
+    </Suspense>
+  );
+}
